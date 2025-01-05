90c3a0f72b7629613b651b2c431cb73634a0ea1e
mikeyQwn <mikeyQwn@gmail.com>
2025-01-06 00:15:08 +0300

    Add lines rope iterator

--- src/rope/iter.rs
+++ src/rope/iter.rs
@@ -1,9 +1,9 @@
 use std::{
-    iter::{FlatMap, Skip, Take},
+    iter::{Enumerate, FlatMap, Peekable, Skip, Take},
     ops::Range,
 };
 
-use super::Node;
+use super::{Node, Rope};
 
 #[derive(Default)]
 pub struct InorderIter<'a> {
@@ -63,6 +63,10 @@ impl<'a> Chars<'a> {
             InorderIter::new(r).flat_map(str::chars);
         Self(out)
     }
+
+    fn lines(self) -> Lines<'a> {
+        Lines::from_raw(self.enumerate().peekable())
+    }
 }
 
 impl Iterator for Chars<'_> {
@@ -86,5 +90,121 @@ impl Iterator for Substring<'_> {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.0.next()
+    }
+}
+
+pub struct Lines<'a> {
+    iter: Peekable<Enumerate<Chars<'a>>>,
+    parse_contents: bool,
+    seen_count: usize,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub struct LineInfo {
+    line_number: usize,
+    character_offset: usize,
+    contents: String,
+}
+
+impl<'a> Lines<'a> {
+    #[must_use]
+    pub fn new(r: &'a Rope) -> Self {
+        let iter = r.chars().enumerate().peekable();
+
+        Self::from_raw(iter)
+    }
+
+    const fn from_raw(iter: Peekable<Enumerate<Chars<'a>>>) -> Self {
+        Self {
+            iter,
+            parse_contents: true,
+            seen_count: 0,
+        }
+    }
+
+    pub fn parse_contents(&mut self, parse_contents: bool) -> &mut Self {
+        self.parse_contents = parse_contents;
+        self
+    }
+}
+
+impl Iterator for Lines<'_> {
+    type Item = LineInfo;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let &(character_offset, _) = self.iter.peek()?;
+        let line_number = self.seen_count;
+        let contents = if self.parse_contents {
+            self.iter
+                .by_ref()
+                .take_while(|&(_, char)| char != '\n')
+                .map(|(_, char)| char)
+                .collect()
+        } else {
+            // Consume line contents
+            self.iter
+                .by_ref()
+                .take_while(|&(_, char)| char != '\n')
+                .for_each(|(_, _)| {});
+            String::new()
+        };
+
+        self.seen_count += 1;
+
+        Some(LineInfo {
+            line_number,
+            character_offset,
+            contents,
+        })
+    }
+
+    fn nth(&mut self, n: usize) -> Option<Self::Item> {
+        let parse_setting = self.parse_contents;
+        self.parse_contents = false;
+
+        for _ in 0..n {
+            let _ = self.next()?;
+        }
+
+        self.parse_contents = parse_setting;
+        self.next()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::rope::Rope;
+
+    use super::*;
+
+    #[test]
+    fn lines() {
+        let inputs = [
+            "",
+            "a\nb\nbc\nd\n",
+            "\n\n\n",
+            "a\n",
+            "\na",
+            "\na\n",
+            "\naba\n",
+            "\n",
+        ];
+        for input in inputs {
+            println!("{:?}", input);
+            let r = Rope::from(String::from(input));
+
+            assert_eq!(
+                Lines::new(&r).map(|i| i.contents).collect::<Vec<_>>(),
+                input.lines().collect::<Vec<_>>()
+            );
+
+            assert_eq!(
+                Lines::new(&r)
+                    .parse_contents(false)
+                    .map(|i| i.contents)
+                    .collect::<Vec<_>>(),
+                input.lines().map(|_| String::new()).collect::<Vec<_>>()
+            );
+        }
     }
 }
